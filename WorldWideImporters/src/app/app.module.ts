c4255093fe5419b60c09430c9be451cda1115167
Óscar Fernández González <ofgnet@msn.com>
2019-06-04 22:10:44 +0200

    Add Footer (Visible Features: rubric72, rubric73, rubric74, rubric75)

--- WorldWideImporters/src/app/app.module.ts
+++ WorldWideImporters/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderLayoutComponent } from './layouts/header-layout/header-layout.component';
+import { FooterLayoutComponent } from './layouts/footer-layout/footer-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderLayoutComponent
+    HeaderLayoutComponent,
+    FooterLayoutComponent
   ],
   imports: [
     BrowserModule
