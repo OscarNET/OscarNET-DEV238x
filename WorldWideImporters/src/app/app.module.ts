1d8365e97897fe2516243bcd5897e29324d935c7
Óscar Fernández González <ofgnet@msn.com>
2019-06-05 22:41:59 +0200

    Add URL Routing (Functional Features: rubric13, rubric34, rubric56, rubric62, rubric64)

--- WorldWideImporters/src/app/app.module.ts
+++ WorldWideImporters/src/app/app.module.ts
@@ -1,17 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
 import { HeaderLayoutComponent } from './layouts/header-layout/header-layout.component';
 import { FooterLayoutComponent } from './layouts/footer-layout/footer-layout.component';
+
+import { AboutComponent } from './shop/about/about.component';
+import { CartComponent } from './shop/cart/cart.component';
+import { ContactComponent } from './shop/contact/contact.component';
+import { HomeComponent } from './shop/home/home.component';
+import { ShoppingComponent } from './shop/shopping/shopping.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderLayoutComponent,
-    FooterLayoutComponent
+    FooterLayoutComponent,
+    AboutComponent,
+    CartComponent,
+    ContactComponent,
+    HomeComponent,
+    ShoppingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      //rubric64 : The about page is accessible at http://localhost:8080/#/about
+      { path: "about", component: AboutComponent },
+
+      //rubric56 : The cart page is accessible at http://localhost:8080/#/cart
+      { path: "cart", component: CartComponent },
+
+      //rubric62 : The contact page is accessible at http://localhost:8080/#/contact
+      { path: "contact", component: ContactComponent },
+
+      //rubric13 : The home page is accessible at http://localhost:8080/#
+      { path: "", component: HomeComponent },
+
+      //rubric34 : The shopping page is accessible at http://localhost:8080/#/shopping
+      { path: "shopping", component: ShoppingComponent },
+
+      { path: "**", redirectTo: "" },
+    ], { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
