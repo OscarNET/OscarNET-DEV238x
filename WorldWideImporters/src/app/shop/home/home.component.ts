3b7142ed170a89cf5dc7171902384a6ddebb328a
Óscar Fernández González <ofgnet@msn.com>
2019-06-09 18:42:33 +0200

    Add Home page: visible and functional features (rubric01, rubric02, rubric03, rubric04, rubric05, rubric07, rubric08, rubric11, rubric12 and rubric13)

--- WorldWideImporters/src/app/shop/home/home.component.ts
+++ WorldWideImporters/src/app/shop/home/home.component.ts
@@ -1,6 +1,12 @@
+/**
+ *  Represents the Home component containing a carousel that cycles through featured products.
+ * 
+ *  Authored by Oscar Fernandez Gonzalez a.k.a. OscarNET
+ */
 import { Component, OnInit } from '@angular/core';
 import { AzureDataService } from '../../services/azure-data.service';
 import { Category } from '../../model/category.model';
+import { Item } from '../../model/item.model';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +15,49 @@ import { Category } from '../../model/category.model';
 })
 export class HomeComponent implements OnInit {
 
-  private categories: Category[];
+  carouselItems: Item[];
 
   constructor(private dataSource: AzureDataService) { }
 
   ngOnInit() {
-    this.getCategories();
+    this.initializeCarousel();
   }
 
-  getCategories = () => {
-    this.dataSource.getCategories().subscribe((response) => {
-      this.categories = response;
-      console.log(this.categories);
+  // Initializes the carousel with the retrieved products from Azure data service.
+  private initializeCarousel() {
+    this.dataSource.getCategories().subscribe((categories) => {
+      this.prepareCarousel(categories);
     }, (error) => {
       alert("The Categories couldn't be retrieved from Azure data service. Error: " + error.statusText);
     });
   }
 
+  // Prepares the carousel containing only the best rating products (using randomness).
+  private prepareCarousel(categories: Category[]) {
+    let bestRatingItems: Item[] = [];
+    let bestRatingItemsByCategory: { [category: string]: Item[]; } = {};
+
+    if ((categories == null) || (categories.length == 0)) {
+      return;
+    }
+
+    categories.forEach(category => {
+      bestRatingItemsByCategory[category.category] = [];
+      category.subcategories.forEach(subcategory => {
+        let bestRatingItemsBySubcategory: Item[] = subcategory.items.filter(item => item.rating == "5");
+        if (bestRatingItemsBySubcategory.length != 0) {
+          let randomItem: Item = bestRatingItemsBySubcategory[Math.floor(Math.random() * bestRatingItemsBySubcategory.length)];
+          bestRatingItems.push(randomItem);
+        }
+      });
+    });
+
+    categories.forEach(category => {
+      bestRatingItemsByCategory[category.category] = bestRatingItems
+        .filter(item => (item.category == category.category))
+        .filter((item, index) => index < 3);
+    });
+
+    this.carouselItems = bestRatingItems;
+  }
 }
