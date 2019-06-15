43b7ba5cd85be3dbc4af7406f6c11ddb6d09e44e
Óscar Fernández González <ofgnet@msn.com>
2019-06-15 19:11:54 +0200

    Add empty Cart page

--- WorldWideImporters/src/app/shop/cart/cart.component.ts
+++ WorldWideImporters/src/app/shop/cart/cart.component.ts
@@ -1,4 +1,10 @@
+/**
+ *  Represents the Cart component for displaying the cart's content.
+ * 
+ *  Authored by Oscar Fernandez Gonzalez a.k.a. OscarNET
+ */
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../../model/item.model';
 
 @Component({
   selector: 'app-cart',
@@ -6,6 +12,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+
+  cartContent: Item[] = [];
 
   constructor() { }
 
